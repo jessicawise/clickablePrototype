62c47b6b1e6609ad86f69550e82f59d14e357c4a
Jessica Wise <jessicawise@Jessicas-MacBook-Air.local>
2018-04-13 09:25:29 -0400

    Refactored JQuery code

--- jquery.js
+++ jquery.js
@@ -1,119 +1,79 @@
-// Map Widget //
 
-$(function() {
-    $(".mapWidgetDefault").resizable({
-        maxHeight: 650,
-        maxWidth: 1350,
-        minHeight: 300,
-        minWidth: 675
-      }).draggable({containment: 'window'
-      });
-  });
+function applyDragResize(selector, config) {
+    var horizontal = false;
+    var dirString = "se"
 
-
-$( "#mapIcon" ).click(function(){
-    $(this).toggleClass("widgetIconExpand")
-    $(".mapWidgetDefault").toggleClass("mapWidgetCollapsed")
-
-    if ($(".mapWidgetDefault").hasClass('mapWidgetCollapsed')) {
-        $(".mapWidgetDefault").resizable({
-            handles: 'e, w'
-        })
-    } else {
-        $(".mapWidgetDefault").resizable({
-            handles: 'e, w, n, s, se'
-        })
+    if (config.maxWidth > config.minWidth) {
+        horizontal = true;
+        dirString += ", e, w"
     }
 
-    $('#map').css("height", "");
+    if (config.maxHeight > config.minHeight) {
+        dirString += ", n, s"
+    }
+
+    //resizor
+    $(selector).resizable({
+        maxHeight: config.maxHeight,
+        maxWidth: config.maxWidth,
+        minHeight: config.minHeight,
+        minWidth: config.minWidth,
+        handles: dirString
+      }).draggable({
+        containment: 'window'
+      });
+
+    //collapser
+      $(selector + " .widgetIconCollapse").click(function(){
+        $(this).toggleClass("widgetIconExpand")
+        $(selector).toggleClass("collapsed")
+    
+        if ($(selector).hasClass("collapsed")) {
+            $(selector).resizable({
+                handles: horizontal ? 'e, w' : 'se'
+            })
+        } else {
+            $(selector).resizable({
+                handles: dirString
+            })
+        }
+
+        $(selector).css("height", "");
+    });
+}
+
+// Map Widget //
+
+applyDragResize("#map", {
+    maxHeight: 650,
+    maxWidth: 1350,
+    minHeight: 300,
+    minWidth: 675
 });
 
 // Video Widget //
 
-$(function() {
-    $(".videoWidgetDefault").resizable({
-        maxHeight: 350,
-        maxWidth: 150,
-        minHeight: 150,
-        minWidth: 150
-      }).draggable({containment: 'window'
-      });
-  });
-
-
-$( "#videoIcon" ).click(function(){
-    $(this).toggleClass("widgetIconExpand")
-    $(".videoWidgetDefault").toggleClass("videoWidgetCollapsed")
-
-    if ($(".videoWidgetDefault").hasClass('videoWidgetCollapsed')) {
-        $(".videoWidgetDefault").resizable({
-            handles: 'e, w'
-        })
-    } else {
-        $(".videoWidgetDefault").resizable({
-            handles: 'e, w, n, s, se'
-        })
-    }
-
-    $('#video').css("height", "");
+applyDragResize("#video", {
+    maxHeight: 350,
+    maxWidth: 150,
+    minHeight: 150,
+    minWidth: 150
 });
 
 // Chat Widget //
 
-$(function() {
-    $(".chatWidgetDefault").resizable({
-        maxHeight: 350,
-        maxWidth: 250,
-        minHeight: 150,
-        minWidth: 250
-      }).draggable({containment: 'window'
-      });
-  });
-
-
-$( "#chatIcon" ).click(function(){
-    $(this).toggleClass("widgetIconExpand")
-    $(".chatWidgetDefault").toggleClass("chatWidgetCollapsed")
-
-    if ($(".chatWidgetDefault").hasClass('chatWidgetCollapsed')) {
-        $(".chatWidgetDefault").resizable({
-            handles: 'e, w'
-        })
-    } else {
-        $(".chatWidgetDefault").resizable({
-            handles: 'e, w, n, s, se'
-        })
-    }
-
-    $('#chat').css("height", "");
+applyDragResize("#chat", {
+    maxHeight: 350,
+    maxWidth: 250,
+    minHeight: 150,
+    minWidth: 250
 });
 
 // Turn Order Widget //
 
-$(function() {
-    $(".turnWidgetDefault").resizable({
-        maxHeight: 350,
-        maxWidth: 150,
-        minHeight: 137,
-        minWidth: 150
-      }).draggable({containment: 'window'
-      });
-  });
-
-
-$( "#turnIcon" ).click(function(){
-    $(this).toggleClass("widgetIconExpand")
-    $(".turnWidgetDefault").toggleClass("turnWidgetCollapsed")
-
-    if ($(".turnWidgetDefault").hasClass('turnWidgetCollapsed')) {
-        $(".turnWidgetDefault").resizable({
-            handles: 'e, w'
-        })
-    } else {
-        $(".turnWidgetDefault").resizable({
-            handles: 'e, w, n, s, se'
-        })
-    }
-
-    $('#turn').css("height", "");
+applyDragResize("#turn", {
+    maxHeight: 350,
+    maxWidth: 150,
+    minHeight: 137,
+    minWidth: 150
 });
